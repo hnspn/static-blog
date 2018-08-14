8a573216ac4a9b55311f0cc1a74dfd2652152543
hnspn <bobmatt911@gmail.com>
2018-08-14 18:22:36 -0400

    started files

---
+++ data/SiteConfig.js
@@ -0,0 +1,56 @@
+module.exports = {
+  blogPostDir: "posts", // The name of directory that contains your posts.
+  blogAuthorDir: "authors", // The name of directory that contains your 'authors' folder.
+  blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
+  siteTitle: "Honeyspoon.me", // Site title.
+  siteTitleAlt: "Honeyspoon blog", // Alternative site title for SEO.
+  siteLogo:
+    "", // "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+  siteUrl: "https://honeyspoon.me", // Domain of your website without pathPrefix.
+  // pathPrefix: "/gatsby-starter-casper", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
+  siteDescription:
+    "I'm a software engineering student and this is my personal blog for all things tech and life related", // Website description used for RSS feeds/meta description tag.
+  siteCover:
+    "https://images.unsplash.com/photo-1423589989400-cc0270157ed0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=68a4521ac2214081b71d4000450b2098&auto=format&fit=crop&w=1950&q=80", 
+    // "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteRssAuthor: "Casper User", // The author name used in the RSS file
+  // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
+  sitePaginationLimit: 10, // The max number of posts per page.
+  googleAnalyticsID: "UA-123949230-1", // GA tracking ID.
+  disqusShortname: "https-honeyspoon-me", // enables Disqus comments, visually deviates from original Casper theme.
+  siteSocialUrls: [
+    "https://github.com/hnspn",
+    "https://twitter.com/hnspn3",
+    "mailto:abderahmane.bouziane@polymtl.ca"
+  ],
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  // Links to social profiles/projects you want to display in the navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/hnspn",
+      iconClassName: "fa fa-github" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/hnspn3",
+      iconClassName: "fa fa-twitter" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Email",
+      url: "mailto:abderahmane.bouziane@polymtl.ca",
+      iconClassName: "fa fa-envelope" // Disabled, see Navigation.jsx
+    }
+  ],
+  // Copyright string for the footer of the website and RSS feed.
+  copyright: {
+    label: "abderahmane bouziane", // Label used before the year
+    year: "2018" // optional, set specific copyright year or range of years, defaults to current year
+    // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
+  },
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+};
